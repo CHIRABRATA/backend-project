caf84fb0772cc4cbe59d9b4813439a14cbfea51c
Chirabrata Ghosal <chirabrataghosal9@gmail.com>
2026-05-03 01:06:27 +0530

    add sendEmail function to handle email sending

--- src/sevices/email.service.js
+++ src/sevices/email.service.js
@@ -22,4 +22,20 @@ transporter.verify((error, success) => {
 }
 );
 
+//function to send email
+async function sendEmail(to, subject, text) {
+    const mailOptions = {
+        from: process.env.EMAIL_USER, 
+        to,
+        subject,
+        text
+    };  
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email: ', error);
+    }
+}
+
 module.exports = transporter;   
